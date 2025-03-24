63b27cad87170034ead585a13fc1c276569ddcf6
BlissMahlathi <162612924+BlissMahlathi@users.noreply.github.com>
2025-03-24 16:24:00 +0200

    Update collapsible.tsx

--- src/components/ui/collapsible.tsx
+++ src/components/ui/collapsible.tsx
@@ -1,5 +1,5 @@
 import * as CollapsiblePrimitive from "@radix-ui/react-collapsible"
-
+ 
 const Collapsible = CollapsiblePrimitive.Root
 
 const CollapsibleTrigger = CollapsiblePrimitive.CollapsibleTrigger
