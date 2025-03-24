22d081c2df2ad094e4885a7783b5fcaf39521863
BlissMahlathi <162612924+BlissMahlathi@users.noreply.github.com>
2025-03-24 16:26:43 +0200

    Update form.tsx

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -20,7 +20,7 @@ type FormFieldContextValue<
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
 > = {
   name: TName
-}
+} 
 
 const FormFieldContext = React.createContext<FormFieldContextValue>(
   {} as FormFieldContextValue
